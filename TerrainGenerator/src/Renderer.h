010204ff5105bd05d614876f3ca564563d548d7a
Zachary Grumbles <zakgrumbles@gmail.com>
2016-04-20 00:26:02 -0500

    Basic renderer set up.

---
+++ TerrainGenerator/src/Renderer.h
@@ -0,0 +1,79 @@
+#include <GL/freeglut.h>
+#include <vector>
+#include <gmtl/Point.h>
+
+namespace Renderer{
+	int w_width;
+	int w_height;
+	int window;
+	float view_angle;
+
+	std::vector<gmtl::Point3f> vertices;
+
+	void display_function(){
+		glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glLoadIdentity();
+		gluPerspective(view_angle, (float)w_width / (float)w_height, 0.1, 30);
+
+
+		glBegin(GL_TRIANGLES);
+		int num_vert = vertices.size();
+		for (int i = 0; i < num_vert; i++){
+			glVertex3f(vertices[i][0], vertices[i][1], vertices[i][2]);
+		}
+
+		glEnd();
+		glutSwapBuffers();
+	}
+
+	void reshape_function(int x, int y){
+		glutReshapeWindow(w_width, w_height);
+	}
+
+	bool init(int window_w, int window_h, float v_angle, int argc = 0, char* argv[] = NULL){
+		glutInit(&argc, argv);
+		glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+
+		if (window_w > 0){
+			w_width = window_w;
+		}
+		else{
+			return false;
+		}
+		if (window_h > 0){
+			w_height = window_w;
+		}
+		else{
+			return false;
+		}
+		if (v_angle > 0){
+			view_angle = v_angle;
+		}
+		else{
+			return false;
+		}
+
+		glutInitWindowPosition(100, 100);
+		glutInitWindowSize(w_width, w_height);
+		window = glutCreateWindow("Procedural Terrain Generation - Zachary Grumbles");
+		glutDisplayFunc(display_function);
+		glutReshapeFunc(reshape_function);
+		glShadeModel(GL_SMOOTH);
+		glEnable(GL_DEPTH_TEST);
+		glPolygonOffset(1, 1);
+
+		vertices = std::vector<gmtl::Point3f>();
+
+		return true;
+	}
+
+	void start(){
+		glutMainLoop();
+	}
+
+	void setVertices(std::vector<gmtl::Point3f> v){
+		vertices.clear();
+		vertices = v;
+	}
+}
--- TerrainGenerator/src/main.cpp
+++ TerrainGenerator/src/main.cpp
@@ -1,6 +1,22 @@
+#include "Renderer.h"
 
+int monitor_width;
+int monitor_height;
+int window_width = 640;
+int window_height = 480;
+int main_window;
 
-int main(){
+int main(int argc, char* argv[]){
+	Renderer::init(window_width, window_height, 45.0f, argc, argv);
+
+	std::vector<gmtl::Point3f> v = std::vector<gmtl::Point3f>();
+	v.push_back(gmtl::Point3f(-0.5, 0.0, -3.0));
+	v.push_back(gmtl::Point3f(0.0, 0.5, -3.0));
+	v.push_back(gmtl::Point3f(0.5, 0.0, -3.0));
+
+	Renderer::setVertices(v);
+
+	Renderer::start();
 
 	return 0;
 }
