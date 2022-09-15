352fce2e86d26de96806dcd7557dce607de9681d
Zak Grumbles <zakgrumbles@gmail.com>
2022-09-15 16:43:15 -0500

    Restructuring; terrain generated and rendered

--- widgets/terrainviewwidget.h
+++ widgets/terrainviewwidget.h
@@ -2,8 +2,10 @@
 #define TERRAINVIEWWIDGET_H
 
 #include <memory>
-#include <QOpenGLWidget>
 #include <QOpenGLFunctions>
+#include <QOpenGLVertexArrayObject>
+#include <QOpenGLBuffer>
+#include <QOpenGLWidget>
 #include <QThread>
 #include <QWidget>
 
@@ -16,6 +18,7 @@ class TerrainViewWidget : public QOpenGLWidget, protected QOpenGLFunctions
 
 public:
     TerrainViewWidget(QWidget* parent);
+    ~TerrainViewWidget();
 
 public slots:
     void generate();
@@ -32,6 +35,9 @@ protected:
     void resizeGL(int w, int h) override;
     void paintGL() override;
 
+    // Helper methods
+    bool compileShaders();
+
     QThread generatorThread;
 
     std::unique_ptr<std::vector<glm::vec3>> terrainVerts;
@@ -40,6 +46,10 @@ protected:
     glm::mat4 projectionMatrix;
     glm::mat4 modelMatrix;
     glm::mat4 viewMatrix;
+
+    unsigned int shaderProg;
+    QOpenGLVertexArrayObject vao;
+    QOpenGLBuffer vbo;
 };
 
 #endif // TERRAINVIEWWIDGET_H
