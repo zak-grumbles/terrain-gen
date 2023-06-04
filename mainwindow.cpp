42bd501bcb56c7897600b643ca2c1d760f6e7802
Zak Grumbles <zakgrumbles@gmail.com>
2023-06-04 16:15:52 -0500

    Exporting from File menu complete
    - Added menu bar to application with File menu
    - Added export action to File menu
    - Added logic to the MainWindow to handle new export heightmap action.
    - Added new public method to HeightmapEditorTab for getting the
      Heightmap.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,9 @@
 #include "mainwindow.h"
+
+#include <QActionGroup>
+#include <QFileDialog>
+#include <QMenuBar>
+
 #include "./ui_mainwindow.h"
 #include "widgets/heightmapeditortab.h"
 #include "widgets/terrainviewwidget.h"
@@ -8,20 +13,22 @@ MainWindow::MainWindow(QWidget *parent)
     , ui_(new Ui::MainWindow)
 {
     ui_->setupUi(this);
-    ui_->statusbar->showMessage("Starting up. . .");
 
-    HeightmapEditorTab* node_view = findChild<HeightmapEditorTab*>();
-    TerrainViewWidget* terrain_view = findChild<TerrainViewWidget*>();
+    node_view_ = findChild<HeightmapEditorTab*>();
+    terrain_view_ = findChild<TerrainViewWidget*>();
 
-    if(node_view != nullptr && terrain_view != nullptr)
+    if(node_view_ != nullptr && terrain_view_ != nullptr)
     {
-        connect(node_view, &HeightmapEditorTab::HeightmapChanged,
-                terrain_view, &TerrainViewWidget::OnHeightmapUpdated);
+        connect(node_view_, &HeightmapEditorTab::HeightmapChanged,
+                terrain_view_, &TerrainViewWidget::OnHeightmapUpdated);
     }
     else
     {
         qWarning() << "CHILDREN NOT FOUND";
     }
+
+    CreateActions_();
+    CreateMenus_();
 }
 
 MainWindow::~MainWindow()
@@ -29,8 +36,51 @@ MainWindow::~MainWindow()
     delete ui_;
 }
 
-void MainWindow::SetStatusMessage(const QString& msg)
+void MainWindow::ExportHeightmap()
 {
-    ui_->statusbar->showMessage(msg);
+    QString filename = QFileDialog::getSaveFileName(this,
+        tr("Export heightmap"), "", tr("Image (*.bmp,*.png);;All Files(*)"));
+
+    if(!filename.isEmpty())
+    {
+        auto heightmap = node_view_->GetHeightmap();
+        QFile dest(filename);
+        if(heightmap != nullptr)
+        {
+            if(dest.open(QIODevice::WriteOnly))
+            {
+                auto heightmap_img = heightmap->toImage();
+                heightmap->save(&dest);
+            }
+            else
+            {
+                // TODO: Error
+            }
+        }
+        else
+        {
+            // TODO: Error
+        }
+    }
+    else
+    {
+        // TODO: Error
+    }
 }
 
+
+void MainWindow::CreateActions_()
+{
+    export_heightmap_ = new QAction(tr("&Export Heightmap"), this);
+    export_heightmap_->setStatusTip(tr("Export the created heightmap"));
+    connect(export_heightmap_, &QAction::triggered, this, &MainWindow::ExportHeightmap);
+
+    heightmap_group_ = new QActionGroup(this);
+    heightmap_group_->addAction(export_heightmap_);
+}
+
+void MainWindow::CreateMenus_()
+{
+    file_menu_ = menuBar()->addMenu(tr("&File"));
+    file_menu_->addAction(export_heightmap_);
+}
